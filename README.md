0abfb62b224702cb8d33666c44c21c3cad592d11
Evgeniy <evgeniy7@gmail.com>
2019-11-30 20:21:54 +0300

    Fix readme

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Steps to run this project:
 
 1. Run `npm i` command
 2. Setup database settings inside `ormconfig.json` file
-3. Run `npm tsc` command
-3. Run `node build\index.js` command
+3. Run `npm run build-client` command
+4. Run `npx tsc` command
+5. Run `node build\index.js` command
--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ createConnection().then(async connection => {
     // start express server
     app.listen(3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+    console.log("Express server has started on port 3000. Open http://localhost:3000 to see results");
 
 }).catch(error => console.log(error));
 
